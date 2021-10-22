15784aa2ff27a267e2e41bc23321d73e926a4a76
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2021-10-22 12:52:48 +0800

    declaration

--- Simulator.cpp
+++ Simulator.cpp
@@ -1,3 +1,25 @@
+/*
+ Copyright © 2021  TokiNoBug
+This file is part of ThreeBodySimulation.
+
+    ThreeBodySimulation is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SlopeCraft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ThreeBodySimulation.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact with me:
+    github:https://github.com/ToKiNoBug
+    bilibili:https://space.bilibili.com/351429231
+*/
+
 #include "Simulator.h"
 
 //const uint32_t BodyCount=3;
@@ -299,14 +321,14 @@ void Simulator::simulateRK4Var1(double step,
 
         double minStep=16*std::nextafter(curTime,curTime*2)-16*curTime;
 
-        bool isOk=true;
+        //bool isOk=true;
 
-        isOk=RK4(step,y,GM,safeDistance,y_h);
+        RK4(step,y,GM,safeDistance,y_h);
         //if(!isOk) { if(noCollide!=nullptr) {*noCollide=isOk; }break;}
 
-        isOk=RK4(step/2,y,GM,safeDistance,y_h_2);
+        RK4(step/2,y,GM,safeDistance,y_h_2);
 
-        isOk=RK4(step/2,y_h_2,GM,safeDistance,y_h_2);
+        RK4(step/2,y_h_2,GM,safeDistance,y_h_2);
 
         if(isErrorTolerantable(y_h,y_h_2)) {
             //error is tolerantable, scale up until next value is untolerantable
